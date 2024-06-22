1fa7821db57ff12b2ff53b4bc3db7e8cf6c3544d
parveenbanurajak <114871708+parveenbanurajak@users.noreply.github.com>
2024-06-22 13:57:40 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
           </div>
         </div>
         <div className="child2">
-          <img src={ai} />
+             <img src={ai} />
         </div>
       </div>
       <div className="items-compny">
